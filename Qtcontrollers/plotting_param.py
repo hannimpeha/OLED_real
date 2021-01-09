49e669807b85cc0155a0acb0a0b304ba09799077
hannimpeha <hannimpeha@gmail.com>
2021-01-09 13:11:53 +0900

    penProject

--- Qtcontrollers/plotting_param.py
+++ Qtcontrollers/plotting_param.py
@@ -1,6 +1,7 @@
 from PyQt5 import QtCore
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QWidget, QGridLayout, QLabel, QComboBox, QTableWidget, QTableWidgetItem
+from PyQt5.QtWidgets import QWidget, QGridLayout, QLabel, QComboBox, QTableWidget, QTableWidgetItem, QLineEdit, \
+    QPushButton, QCheckBox
 
 logo_image = '/Users/hannahlee/PycharmProjects/penProject/controllers/resources/Logo.png'
 
@@ -10,9 +11,11 @@ class Plotting_Param(QWidget):
         layout = QGridLayout()
         logo_image = self.logo_image()
         plotting = Plotting()
+        exportation = Exportation()
 
         layout.addWidget(logo_image, 0, 0)
         layout.addWidget(plotting, 1, 0)
+        layout.addWidget(exportation, 2, 0)
         self.setLayout(layout)
 
     def logo_image(self):
@@ -100,10 +103,58 @@ class Plotting(QWidget):
         self.y = ["50", "50"]
         self.tempList = [[self.x, self.y]]
         self.num_row = len(self.tempList)
-        self.table.setFixedSize(600, 300)
+        self.table.setFixedSize(600, 600)
 
         for i in range(len(self.x)):
             self.num_row = i
             self.table.setItem(self.num_row, 0, QTableWidgetItem(self.x[i]))
             self.table.setItem(self.num_row, 1, QTableWidgetItem(self.y[i]))
         return self.table
+
+class Exportation(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        layout = QGridLayout()
+
+        label = QLabel()
+        label.setText("Exportation")
+        layout.addWidget(label, 0, 0)
+
+        lineEdit = QLineEdit()
+        lineEdit.setFixedSize(100, 20)
+        layout.addWidget(lineEdit, 1, 1)
+
+        btn = QPushButton()
+        btn.setText("Browse")
+        layout.addWidget(btn, 1, 2)
+
+        label = QLabel()
+        label.setText("Path: ")
+        layout.addWidget(label, 1, 0)
+
+        label = QLabel()
+        label.setText("Type: ")
+        layout.addWidget(label, 2, 0)
+
+        check_text = QCheckBox()
+        check_text.setText("Text")
+        layout.addWidget(check_text, 2, 1)
+
+        check_image = QCheckBox()
+        check_image.setText("Image")
+        layout.addWidget(check_image, 2, 2)
+
+        label = QLabel()
+        label.setText("Name: ")
+        layout.addWidget(label, 3, 0)
+
+        lineEdit = QLineEdit()
+        lineEdit.setFixedSize(100, 20)
+        layout.addWidget(lineEdit, 3, 1)
+
+        btn = QPushButton()
+        btn.setText("Export")
+        layout.addWidget(btn, 4, 2)
+
+        self.setLayout(layout)
